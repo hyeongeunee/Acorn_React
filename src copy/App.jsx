53984027ab0c4956f77d9618366f37583b20ee7b
hyeongeun <hhg7899@gmail.com>
2023-08-23 15:11:04 +0900

    fetch & axios

--- src copy/App.jsx
+++ src copy/App.jsx
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-import PostComponent from './components/PostComponent';
-import PostComponent2 from './components/PostComponent2';
 
 class App extends Component {
     //render() 메소드 에서 리턴해주는 jsx 를 활용해서 화면 구성이 된다.
@@ -8,8 +6,6 @@ class App extends Component {
         return (
             <div className="container">
                 <h1>인덱스 페이지 입니다.</h1>
-                <PostComponent />
-                <PostComponent2 />
             </div>
         );
     }
